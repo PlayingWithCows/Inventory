4807b17654fdacdff63a209e92a68e77cb6c9cd1
PlayingWithCows <wiealbern@gmail.com>
2016-10-02 10:57:56 +0200

    initial

---
+++ Assets/CanvasManager.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CanvasManager : MonoBehaviour {
+	private Canvas inventory, hotbar;
+	private bool bagOpen = false;
+	// Use this for initialization
+	void Start () {
+		inventory = GameObject.Find ("InventoryCanvas").GetComponent<Canvas>();
+		hotbar = GameObject.Find ("HotbarCanvas").GetComponent<Canvas>();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown ("Inventory")) {
+
+			if (bagOpen) 
+			{
+				inventory.enabled = false;
+				bagOpen = false;
+			}
+			else
+			{
+				inventory.enabled = true;
+				bagOpen = true;
+			}
+
+		}
+	}
+}
