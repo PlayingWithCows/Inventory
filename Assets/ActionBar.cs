2b105ff819bcefb05ed01175c5c107f5fb581153
PlayingWithCows <wiealbern@gmail.com>
2016-10-03 13:32:25 +0200

    ActionbarSlots

--- Assets/ActionBar.cs
+++ Assets/ActionBar.cs
@@ -1,15 +1,24 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class ActionBar : MonoBehaviour {
+	private Transform hotBarWindow;
 
+	public int hotBarSlotsNumber = 8;
+	public List<Transform> hotBarSlots;
+	public Transform hotBarSlot, inventoryItem;
 	// Use this for initialization
 	void Start () {
-	
+		hotBarWindow = GameObject.Find ("HotbarPanel").transform;
+
+		for(int i = 0; i<hotBarSlotsNumber; i++){
+			Transform newSlot = Instantiate(hotBarSlot);
+			newSlot.transform.SetParent(hotBarWindow);
+			newSlot.transform.name = "Inventory Slot " + i;
+			hotBarSlots.Add (newSlot);
+		}
+
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+
 }
